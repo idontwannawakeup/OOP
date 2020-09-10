27a389a37aa2738ed1a6702bc6b4520f191a8e05
Семенюк Василь <givilur@gmail.com>
2020-09-10 20:30:26 +0300

    Add 05_DateModifier

---
+++ 03_DefiningClasses/05_DateModifier/DateModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05_DateModifier
+{
+    class Program
+    {
+        class DateModifier
+        {
+            private int difference;
+
+            public int Difference { get => difference; }
+
+            public void CalculateDifference(string lhs, string rhs)
+            {
+                DateTime lhsD = DateTime.Parse(lhs);
+                DateTime rhsD = DateTime.Parse(rhs);
+                difference = Math.Abs((lhsD - rhsD).Days);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string lhs = Console.ReadLine();
+            string rhs = Console.ReadLine();
+
+            DateModifier dm = new DateModifier();
+            dm.CalculateDifference(lhs, rhs);
+            Console.WriteLine(dm.Difference);
+
+            Console.ReadKey();
+        }
+    }
+}
