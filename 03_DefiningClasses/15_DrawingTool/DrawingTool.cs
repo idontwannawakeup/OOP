3eabacec2b356778c8b72170cc19564945734f10
Семенюк Василь <givilur@gmail.com>
2020-09-23 19:35:57 +0300

    Add 15_DrawingTool

---
+++ 03_DefiningClasses/15_DrawingTool/DrawingTool.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace _15_DrawingTool
+{
+    class Program
+    {
+        class Figure
+        {
+            public int Width { get; protected set; }
+            public int Height { get; protected set; }
+        }
+
+        class Rectangle : Figure
+        {
+            public Rectangle(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+        }
+
+        class Square : Figure
+        {
+            public Square(int size)
+            {
+                Width = Height = size;
+            }
+        }
+
+        class DrawingTool
+        {
+            private Figure figureToDraw;
+
+            public void Draw()
+            {
+                for (int i = 0; i < figureToDraw.Height; i++)
+                {
+                    Console.Write('|');
+                    char value = (i == 0 || i == figureToDraw.Height - 1) ? '-' : ' ';
+                    for (int j = 0; j < figureToDraw.Width; j++)
+                    {
+                        Console.Write(value);
+                    }
+                    Console.WriteLine('|');
+                }
+            }
+
+            public DrawingTool(Figure figure)
+            {
+                figureToDraw = figure;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            Figure figureToDraw;
+            if (type.Equals("Square"))
+            {
+                int size = int.Parse(Console.ReadLine());
+                figureToDraw = new Square(size);
+            }
+            else
+            {
+                int width = int.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                figureToDraw = new Rectangle(width, height);
+            }
+
+            DrawingTool dt = new DrawingTool(figureToDraw);
+            dt.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
