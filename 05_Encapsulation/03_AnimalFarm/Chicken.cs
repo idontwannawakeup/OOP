1d321e0c6ac8c5b72176992bdaa32e3520bad305
Семенюк Василь <givilur@gmail.com>
2020-10-12 17:52:03 +0300

    Add 03_AnimalFarm

---
+++ 05_Encapsulation/03_AnimalFarm/Chicken.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _03_AnimalFarm
+{
+    class Chicken
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value == null || value == "" || value == " ")
+                    throw new Exception("Name cannot be empty.");
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0 || value > 15)
+                    throw new Exception("Age should be between 0 and 15.");
+
+                age = value;
+            }
+        }
+
+        public int ProductPerDay { get => CalculateProductPerDay(); }
+
+        private int CalculateProductPerDay()
+        {
+            return Age / 10;
+        }
+
+        public Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ 05_Encapsulation/03_AnimalFarm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03_AnimalFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chickenName = Console.ReadLine();
+            int chickenAge = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var chicken = new Chicken(chickenName, chickenAge);
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) " +
+                                  $"can produce {chicken.ProductPerDay} eggs per day.");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
