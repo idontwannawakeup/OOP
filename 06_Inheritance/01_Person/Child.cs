e71f6c2a5c8041f332f207d9bc1ac3103fd3fae8
Семенюк Василь <givilur@gmail.com>
2020-10-15 17:43:30 +0300

    Add 06_Inheritance, 06_01_Person

---
+++ 06_Inheritance/01_Person/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_Person
+{
+    class Child : Person
+    {
+        public override int Age
+        {
+            get => base.Age;
+            set
+            {
+                if (value >= 15)
+                    throw new ArgumentException("Child's age must be less than 15!");
+
+                base.Age = value;
+            }
+        }
+
+        public Child(string name, int age)
+            : base(name, age)
+        { }
+    }
+}
---
+++ 06_Inheritance/01_Person/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace _01_Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public virtual string Name
+        {
+            get => name;
+            set
+            {
+                if (value.Length < 3)
+                    throw new ArgumentException("Name's length should not be" +
+                                                " less than 3 symbols!");
+
+                name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Age must be positive!");
+
+                age = value;
+            }
+        }
+
+        public override string ToString() => $"Name: {Name}, Age: {Age}";
+        //{
+        //    return $"Name: {Name}, Age: {Age}";
+        //}
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
