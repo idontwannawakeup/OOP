dee21377183df1ddc5f096742834780335279fbf
Семенюк Василь <givilur@gmail.com>
2020-10-27 21:39:37 +0200

    Add solution for 07_Interfaces and 01

---
+++ 07_InterfacesAndAbstraction/01_DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_DefineAnInterfaceIPerson
+{
+    class Citizen : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ 07_InterfacesAndAbstraction/01_DefineAnInterfaceIPerson/IPerson.cs
@@ -0,0 +1,8 @@
+﻿namespace _01_DefineAnInterfaceIPerson
+{
+    interface IPerson
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+}
