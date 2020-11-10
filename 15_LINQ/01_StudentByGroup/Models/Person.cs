ab646b9dd97765f19c8809a2a2edb6565bac48e0
Семенюк Василь <givilur@gmail.com>
2020-11-10 14:58:57 +0200

    Add solution for 15_LINQ, add 01 from 15

---
+++ 15_LINQ/01_StudentByGroup/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace _01_StudentByGroup.Models
+{
+    class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Group { get; }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        public static Person Parse(string input)
+        {
+            string[] info = input.Split(' ');
+            return new Person(info[0], info[1], info[2]);
+        }
+
+        public Person(string firstName, string lastName)
+            : this(firstName, lastName, 0)
+        { }
+
+        public Person(string firstName, string lastName, string group)
+            : this(firstName, lastName, int.Parse(group))
+        { }
+
+        public Person(string firstName, string lastName, int group)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Group = group;
+        }
+    }
+}
---
+++ 15_LINQ/01_StudentByGroup/Program.cs
@@ -0,0 +1,29 @@
+﻿using _01_StudentByGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_StudentByGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Person>();
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                students.Add(Person.Parse(input));
+                input = Console.ReadLine();
+            }
+
+            var studentsFromGroup2 = students.Where(element => element.Group == 2)
+                                             .OrderBy(element => element.FirstName);
+
+            foreach (var item in studentsFromGroup2)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
