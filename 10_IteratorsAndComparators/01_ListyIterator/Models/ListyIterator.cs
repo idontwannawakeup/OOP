4ae8bede2bae3f8bd7ec978deb3592508cd5a657
Семенюк Василь <givilur@gmail.com>
2020-11-16 21:36:12 +0200

    Added solution for 10, added 01 from 10

---
+++ 10_IteratorsAndComparators/01_ListyIterator/Models/ListyIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_ListyIterator.Models
+{
+    class ListyIterator<T>
+    {
+        private List<T> data;
+        private int current;
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                current++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext() => (current < data.Count - 1);
+
+        public void Print()
+        {
+            if (data.Count == 0)
+                throw new InvalidOperationException("Invalid operation!");
+
+            Console.WriteLine(data[current]);
+        }
+
+        public ListyIterator(List<T> data)
+        {
+            this.data = data;
+            current = 0;
+        }
+    }
+}
---
+++ 10_IteratorsAndComparators/01_ListyIterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using _01_ListyIterator.Models;
+using System;
+using System.Linq;
+
+namespace _01_ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').ToList();
+            input.Remove("Create");
+            var iterator = new ListyIterator<string>(input);
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+                try
+                {
+                    switch (command)
+                    {
+                        case "Move": Console.WriteLine(iterator.Move()); break;
+                        case "HasNext": Console.WriteLine(iterator.HasNext()); break;
+                        case "Print": iterator.Print(); break;
+                    }
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
