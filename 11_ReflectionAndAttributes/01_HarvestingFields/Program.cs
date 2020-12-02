c7f4d78c080a5ad54e3e19e8b7f38896ee03f68f
Семенюк Василь <givilur@gmail.com>
2020-12-03 00:08:09 +0200

    Added solution for 11, added 01 from 11

---
+++ 11_ReflectionAndAttributes/01_HarvestingFields/Program.cs
@@ -0,0 +1,51 @@
+﻿using _01_HarvestingFields.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace _01_HarvestingFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "HARVEST")
+            {
+                var fields = input switch
+                {
+                    "private" => typeof(HarvestingFields)
+                        .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                        .Where(x => x.IsPrivate)
+                        .ToList(),
+
+                    "public" => typeof(HarvestingFields)
+                        .GetFields()
+                        .ToList(),
+
+                    "protected" => typeof(HarvestingFields)
+                        .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                        .Where(x => !x.IsPrivate)
+                        .ToList(),
+
+                    "all" => typeof(HarvestingFields)
+                        .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                        .ToList(),
+
+                    _ => null
+                };
+
+                fields?.ForEach(element =>
+                {
+                    string modifier = element.IsPublic ? "public"
+                        : element.IsPrivate ? "private" : "protected";
+
+                    Console.WriteLine($"{modifier} {element.FieldType.Name} {element.Name}");
+                });
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
