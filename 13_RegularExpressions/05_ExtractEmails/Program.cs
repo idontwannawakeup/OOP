41aeb1d3cdb3044a0b9c2845ba1df6c64b1d1543
Семенюк Василь <givilur@gmail.com>
2020-12-03 19:37:37 +0200

    Added 05, 06 from 13

---
+++ 13_RegularExpressions/05_ExtractEmails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _05_ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string emailPattern =
+                @"\b(?<![-._])[a-zA-Z0-9]+[-._]?[a-zA-Z0-9]+(?!=[-._])\b@\b(?<![-._])[\w-]+\.[\w-.]+\b";
+
+            var lineBuilder = new StringBuilder();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+                lineBuilder.AppendLine(input);
+
+            string line = lineBuilder.ToString();
+            foreach (Match item in Regex.Matches(line, emailPattern))
+                Console.WriteLine(item.Value);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 13_RegularExpressions/06_SentenceExtractor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _06_SentenceExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentencePattern = @"[\w\s\–’]*[.!?]";
+            string wordPattern = @"[\w\–’]*(?=[\s.?!])";
+
+            string keyword = Console.ReadLine();
+
+            var lineBuilder = new StringBuilder();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+                lineBuilder.Append(input);
+
+            string line = lineBuilder.ToString();
+            foreach (Match sentence in Regex.Matches(line, sentencePattern))
+            {
+                var words = Regex.Matches(sentence.Value, wordPattern)
+                    .Select(element => element.Value)
+                    .ToList();
+
+                if (words.Contains(keyword))
+                    Console.WriteLine(sentence.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
