beaf5f22ef6c5980aa742e8c7e8b5eb483c11244
Семенюк Василь <givilur@gmail.com>
2020-12-03 23:09:30 +0200

    Added 07, 08 from 13, added input.txt files

---
+++ 13_RegularExpressions/07_ValidUsernames/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _07_ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string usernamePattern = @"\b[a-zA-z]\w{2,24}\b";
+
+                string input = Console.ReadLine();
+                if (input.Length > 9999)
+                    throw new Exception("Invalid input length");
+
+                var usernames = input
+                    .Split(' ', '/', '\\', '(', ')')
+                    .ToList();
+
+                var validUsernames = usernames
+                    .Where(element => Regex.IsMatch(element, usernamePattern))
+                    .ToList();
+
+                string maxLhs = (validUsernames.Count == 1)
+                    ? validUsernames[0]
+                    : string.Empty;
+
+                string maxRhs = string.Empty;
+
+                for (int i = 0; i < validUsernames.Count - 1; i++)
+                    if ((maxLhs.Length + maxRhs.Length) <
+                        (validUsernames[i].Length + validUsernames[i + 1].Length))
+                    {
+                        maxLhs = validUsernames[i];
+                        maxRhs = validUsernames[i + 1];
+                    }
+
+                Console.WriteLine(maxLhs);
+                Console.WriteLine(maxRhs);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 13_RegularExpressions/08_SemanticHTML/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08_SemanticHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = $"<div\\s*\\n*(.*?)\\s*\\n*(id|class)\\s*\\n*=\\s*\\n*\"(main|header|nav|article|section|aside|footer)\"\\s*\\n*(.*?)\\s*\\n*>(.*)<\\/div> <!--\\s*\\3\\s*-->";
+            var lineBuilder = new StringBuilder();
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+                lineBuilder.AppendLine(input);
+
+            string line = lineBuilder.ToString();
+            while (Regex.IsMatch(line, pattern, RegexOptions.Singleline))
+            {
+                line = Regex.Replace(line, pattern, (match) =>
+                {
+                    string tag = match.Groups[3].Value;
+
+                    string beforeId = string.IsNullOrWhiteSpace(match.Groups[1].Value)
+                        ? string.Empty
+                        : $" {match.Groups[1].Value}";
+
+                    string afterId = string.IsNullOrWhiteSpace(match.Groups[4].Value)
+                        ? string.Empty
+                        : $" {match.Groups[4].Value}";
+
+                    string content = match.Groups[5].Value;
+
+                    return $"<{tag}{beforeId}{afterId}>{content}</{tag}>";
+                }, RegexOptions.Singleline);
+            }
+
+            Console.WriteLine(line);
+
+            Console.ReadKey();
+        }
+    }
+}
